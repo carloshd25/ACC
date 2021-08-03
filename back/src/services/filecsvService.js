524f0b3dd53a1c78a387ab2b34b1be7adf863106
Carlos Arturo Orrego <cartpower15@gmail.com>
2021-08-03 03:49:40 +0000

    Validacion de formato y estructura

--- back/src/services/filecsvService.js
+++ back/src/services/filecsvService.js
@@ -4,11 +4,20 @@ exports.ProcessFile = async (textFile) => {
     const arrayFilas = textFile.split('\n');
     var cedula, cedulaSHA, filaFin;
     let textFin = 'cedula,cedula_hash,nombre\n';
-    for (var i = 1; i < arrayFilas.length; i++) {
-        cedula = arrayFilas[i].split(',');
-        cedulaSHA = computeSHA256(cedula[0]);
-        filaFin = cedula[0] + ',' + cedulaSHA + ',' + cedula[1];
-        textFin = textFin + filaFin + '\n';
+    cedula = arrayFilas[0].split(',');
+    if(cedula.length == 2 && cedula[0] == 'cedula' && cedula[1] == 'cedula'){
+        for (var i = 1; i < arrayFilas.length; i++) {
+            cedula = arrayFilas[i].split(',');
+            if(cedula.length == 2){
+                cedulaSHA = computeSHA256(cedula[0]);
+                filaFin = cedula[0] + ',' + cedulaSHA + ',' + cedula[1];
+                textFin = textFin + filaFin + '\n';
+            } else {
+                throw new GeneralException('Archivo no tiene estructura valida, error en la fila: ' + i + 1, 401);
+            }
+        }
+    } else {
+        throw new GeneralException('Archivo no tiene estructura valida, encabezado con error', 401);
     }
     return textFin;
 };
